f22f975cf65e618c30ddf1c173c5f1e1019fd997
jpatton638 <jpatton638@users.noreply.github.com>
2023-11-10 14:15:52 +0000

    DDCYLS-3198 | Dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,22 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
+  private val bootstrapVersion = "7.23.0"
+
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"   %% "play-conditional-form-mapping" % "1.12.0-play-28",
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28"    % "7.15.0",
+    "uk.gov.hmrc"   %% "play-conditional-form-mapping" % "1.13.0-play-28",
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28"    % bootstrapVersion,
     "uk.gov.hmrc"   %% "secure"                        % "8.1.0",
-    "org.typelevel" %% "cats-core"                     % "2.9.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "7.0.0-play-28"
+    "org.typelevel" %% "cats-core"                     % "2.10.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc"            % "7.26.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "7.15.0",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % bootstrapVersion,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
     "org.pegdown"             % "pegdown"                  % "1.6.0",
-    "org.jsoup"               % "jsoup"                    % "1.15.3",
+    "org.jsoup"               % "jsoup"                    % "1.15.4",
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current,
     "org.mockito"             % "mockito-all"              % "2.0.2-beta",
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0",
@@ -27,7 +29,7 @@ object AppDependencies {
   ).map(_ % "test,it")
 
   val overrides: Seq[ModuleID] = Seq(
-    "commons-codec" % "commons-codec" % "1.15"
+    "commons-codec" % "commons-codec" % "1.16.0"
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
