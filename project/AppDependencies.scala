74dfb49943d8dfde9b45eed7c11959d507d8eda1
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-04-11 11:16:35 +0100

    Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,12 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"   %% "play-conditional-form-mapping-play-30" % "3.2.0",
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"   %% "secure"                                % "8.2.0" exclude("org.bouncycastle", "bcprov-jdk15on"),
-    "org.typelevel" %% "cats-core"                             % "2.10.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"            % "11.12.0",
-    "org.bouncycastle" % "bcprov-jdk18on"                      % "1.80"
+    "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"            % bootstrapVersion,
+    "uk.gov.hmrc"     %% "secure"                                % "8.2.0" exclude("org.bouncycastle", "bcprov-jdk15on"),
+    "org.typelevel"   %% "cats-core"                             % "2.10.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"            % "11.13.0",
+    "org.bouncycastle" % "bcprov-jdk18on"                        % "1.80"
   )
 
   val test: Seq[ModuleID] = Seq(
