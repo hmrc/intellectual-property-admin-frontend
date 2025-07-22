71c2ac96a187368d16a10673b0700b4c33894176
Bon Parker <45689313+bonparker@users.noreply.github.com>
2025-07-22 13:45:27 +0100

    DDCYLS-6991: Bootstrap 9.18.0 upgrade (#51)
    * DDCYLS-6991: Bootstrap 9.18.0 upgrade
    
    * DDCYLS-6991: App Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.18.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                 %% "play-conditional-form-mapping-play-30" % "3.3.0",
-    "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"                 %% "secure"                                % "8.2.0" exclude("org.bouncycastle", "bcprov-jdk15on"),
-    "org.typelevel"               %% "cats-core"                             % "2.10.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"            % "12.6.0",
-    "org.bouncycastle"             % "bcprov-jdk18on"                        % "1.80",
-    "org.jsoup"                    % "jsoup"                                 % "1.15.3"       % Test,
-    "org.scalatest"               %% "scalatest"                             % "3.2.16"       % Test,
-    "org.scalatestplus.play"      %% "scalatestplus-play"                    % "5.1.0"        % Test
+    "uk.gov.hmrc"            %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"            %% "bootstrap-frontend-play-30"            % bootstrapVersion,
+    "uk.gov.hmrc"            %% "secure"                                % "8.2.0" exclude ("org.bouncycastle", "bcprov-jdk15on"),
+    "org.typelevel"          %% "cats-core"                             % "2.10.0",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30"            % "12.7.0",
+    "org.bouncycastle"        % "bcprov-jdk18on"                        % "1.80",
+    "org.jsoup"               % "jsoup"                                 % "1.15.3" % Test,
+    "org.scalatest"          %% "scalatest"                             % "3.2.16" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"                    % "5.1.0"  % Test
   )
 
   val test: Seq[ModuleID] = Seq(
