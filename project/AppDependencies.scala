090faf9c5b8bcbaed46a0fa39dc9bd63744b1128
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2026-02-13 14:55:04 +0000

    DDCYLS-8050: App dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"            %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"            %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"            %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"            %% "secure"                                % "8.2.0" exclude ("org.bouncycastle", "bcprov-jdk15on"),
     "org.typelevel"          %% "cats-core"                             % "2.10.0",
-    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30"            % "12.8.0",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30"            % "12.31.0",
     "org.bouncycastle"        % "bcprov-jdk18on"                        % "1.80",
     "org.jsoup"               % "jsoup"                                 % "1.15.3" % Test,
     "org.scalatest"          %% "scalatest"                             % "3.2.16" % Test,
