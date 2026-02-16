f9b7b33cca93a7a7d659d6386850275138625287
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2026-02-16 13:38:31 +0000

    DDCYLS-8050: Resolving security vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,18 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"            %% "play-conditional-form-mapping-play-30" % "3.4.0",
-    "uk.gov.hmrc"            %% "bootstrap-frontend-play-30"            % bootstrapVersion,
+    "uk.gov.hmrc"            %% "bootstrap-frontend-play-30"            % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
     "uk.gov.hmrc"            %% "secure"                                % "8.2.0" exclude ("org.bouncycastle", "bcprov-jdk15on"),
     "org.typelevel"          %% "cats-core"                             % "2.10.0",
     "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30"            % "12.31.0",
     "org.bouncycastle"        % "bcprov-jdk18on"                        % "1.80",
     "org.jsoup"               % "jsoup"                                 % "1.15.3" % Test,
     "org.scalatest"          %% "scalatest"                             % "3.2.16" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"                    % "5.1.0"  % Test
+    "org.scalatestplus.play" %% "scalatestplus-play"                    % "5.1.0"  % Test,
+    "org.apache.commons"      % "commons-lang3"                         % "3.18.0",
+    "ch.qos.logback"          % "logback-core"                          % "1.5.27",
+    "ch.qos.logback"          % "logback-classic"                       % "1.5.27",
+    "at.yawk.lz4"             %  "lz4-java"                             % "1.10.3"
   )
 
   val test: Seq[ModuleID] = Seq(
