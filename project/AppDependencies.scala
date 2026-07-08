3dde19b1d435a1e68ca568ba5c429d96e1b21b87
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2026-07-08 15:45:48 +0100

    DDCYLS-8363: play frontend and bootstrap upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-frontend-play-30"            % bootstrapVersion exclude ("org.apache.commons", "commons-lang3"),
     "uk.gov.hmrc"            %% "secure"                                % "8.2.0" exclude ("org.bouncycastle", "bcprov-jdk15on"),
     "org.typelevel"          %% "cats-core"                             % "2.10.0",
-    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30"            % "13.9.0",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30"            % "13.10.0",
     "org.bouncycastle"        % "bcprov-jdk18on"                        % "1.80",
     "ch.qos.logback"          % "logback-core"                          % "1.5.21",
     "at.yawk.lz4"             % "lz4-java"                              % "1.10.3",
